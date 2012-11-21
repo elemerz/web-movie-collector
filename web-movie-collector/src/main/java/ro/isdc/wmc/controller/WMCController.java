c21bb7b8a0767b17c7f08e9c84821fc5c692eaef
elemer.zagoni <elemerz@gmail.com>
2012-11-21 18:31:11 +0200

    Language Changer component implemented.

--- web-movie-collector/src/main/java/ro/isdc/wmc/controller/WMCController.java
+++ web-movie-collector/src/main/java/ro/isdc/wmc/controller/WMCController.java
@@ -3,6 +3,8 @@ package ro.isdc.wmc.controller;
 import java.io.IOException;
 import java.util.Set;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.atmosphere.cpr.AtmosphereResource;
 import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.JsonParseException;
@@ -14,10 +16,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 
 import ro.isdc.wmc.business.IMovieRetrieverBusinessManager;
 import ro.isdc.wmc.controller.util.AtmosphereUtil;
@@ -30,7 +35,7 @@ import ro.isdc.wmc.model.WebsitesXPATHMapper;
  * Handles requests for the application home page.
  */
 @Controller
-public class WMCController {
+public class WMCController extends LocaleAwareController{
 	
 	@Autowired
 	private InfoSourceConfig infoSourceConfig;
@@ -49,12 +54,10 @@ public class WMCController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(final Model model) {	 
-	    
-	    Set<String> infoSources = infoSourceConfig.getSiteConfig().getConfigMap().keySet();
+		Set<String> infoSources = infoSourceConfig.getSiteConfig().getConfigMap().keySet();
 		model.addAttribute("infoSources", infoSources);
 		return "searchPage"; 
 	}
-	
 	
 	/**
 	 * The method where the atmosphere requests arrive.
