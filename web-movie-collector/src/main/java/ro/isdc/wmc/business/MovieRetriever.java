b669db953280aa7a910f1e5554ae94d5a7c897cd
Ioana.Mocan <Ioana.Mocan@ISDC30508.isdcnet.local>
2012-11-21 14:01:47 +0200

    added test functionality

--- web-movie-collector/src/main/java/ro/isdc/wmc/business/MovieRetriever.java
+++ web-movie-collector/src/main/java/ro/isdc/wmc/business/MovieRetriever.java
@@ -15,30 +15,28 @@ import org.apache.http.nio.reactor.IOReactorException;
 import org.apache.http.params.CoreConnectionPNames;
 import org.apache.http.util.EntityUtils;
 import org.atmosphere.cpr.AtmosphereResource;
+import org.springframework.stereotype.Component;
 
 import ro.isdc.wmc.model.SimpleMovie;
 import ro.isdc.wmc.model.WebsitesXPATHMapper;
 import ro.isdc.wmc.parser.impl.SourceParserImpl;
 
+@Component
 public class MovieRetriever  {
 
 	private final HttpHost proxy;
 	private HttpAsyncClient httpclient;
 	
-	public MovieRetriever(List<HttpUriRequest> requests) throws IOReactorException {
+	public MovieRetriever() throws IOReactorException {
 		this.proxy  = new HttpHost("172.17.0.10", 8080) ;
-		this.httpclient = new DefaultHttpAsyncClient();
-		initParams();
 		
 	}
 
-	public  MovieRetriever(List<HttpUriRequest> requests , AtmosphereResource atmoResource ) throws IOReactorException {
-		this.proxy  = new HttpHost("172.17.0.10", 8080) ;
+	public void execute(List<HttpUriRequest> requests, AtmosphereResource atmoResource,   final WebsitesXPATHMapper  websitesXPATHMapper) throws InterruptedException, IOReactorException  {
+		
 		this.httpclient = new DefaultHttpAsyncClient();
+		this.httpclient.start();
 		initParams();
-	}	
-
-	public void execute(List<HttpUriRequest> requests, AtmosphereResource atmoResource,   final WebsitesXPATHMapper  websitesXPATHMapper) throws InterruptedException  {
 		final CountDownLatch latch =  new CountDownLatch(requests.size());
 		try {
 
@@ -92,6 +90,54 @@ public class MovieRetriever  {
 		}
 		System.out.println("Done");
 	}
+	
+	public void execute(final HttpUriRequest request) throws InterruptedException  {
+		try{
+			System.out.println("inainte de httpclient: " + Thread.currentThread().getName());
+		httpclient.execute(request, new FutureCallback<HttpResponse>() {
+
+			public void completed(final HttpResponse response) {
+				
+				
+				//TODO: Pass the parser the html with 
+				try {							
+					 String responseAsString = EntityUtils.toString(response.getEntity());
+					 SourceParserImpl parser = new SourceParserImpl();
+					 String uri = request.getURI().getHost();
+					 uri = uri.subSequence(uri.indexOf('.') + 1, uri.lastIndexOf('.')).toString();
+					 
+					 //ArrayList<SimpleMovie> movies = (ArrayList<SimpleMovie>) parser.getSimpleMovieListFromSite(responseAsString, uri, websitesXPATHMapper);
+					 
+					/* for (SimpleMovie item : movies) {
+						 System.out.println(item.getTitle());
+					}*/
+					 
+					//TODO: Broadcast the response to client by using the atmoResource broadcaster
+					 
+				/*	 atmoResource.getBroadcaster().broadcast(arg0);
+					 resultOBJ.setBasicMoviesArray(movies); 		*/					
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+
+			public void failed(final Exception ex) {
+				System.out.println(request.getRequestLine() + "->" + ex);
+			}
+
+			public void cancelled() {
+				System.out.println(request.getRequestLine() + " cancelled");
+			}
+
+		});	
+		} finally {
+			System.out.println("httpclient shut down: " + Thread.currentThread().getName());
+			httpclient.shutdown();
+			
+		}
+	}
+
 
 
 
@@ -101,7 +147,7 @@ public class MovieRetriever  {
 		.setIntParameter(CoreConnectionPNames.CONNECTION_TIMEOUT, 3000)
 		.setIntParameter(CoreConnectionPNames.SOCKET_BUFFER_SIZE, 8 * 1024)
 		.setBooleanParameter(CoreConnectionPNames.TCP_NODELAY, true).setParameter(ConnRoutePNames.DEFAULT_PROXY, proxy);
-		this.httpclient.start();
+		
 	}
 
 
