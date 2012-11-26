b11eea34ee049f111973c519396b506fc0cd8e02
elemer.zagoni <elemer.zagoni@lamark.mmbase.host>
2012-11-26 16:25:11 +0200

    Added new theme files.

---
+++ web-movie-collector/src/main/webapp/resources/localeChanger/js/localeChanger.js
@@ -0,0 +1,47 @@
+(function($, NS, SuperClass, SubClass) {
+	window[NS][SubClass] = window[NS][SubClass] || window[NS][SuperClass].extend({
+		$ctx: $('#searchPage'),
+		toString : function() {
+			return NS + '.' + SubClass;
+		},
+		/** Constructor. */
+		init : function(cfg) {
+			window.setTimeout(this.buildMenu,1000);
+		},
+		buildMenu:function(){
+			$( "#rerun" )
+            .button()
+            .click(function() {
+                console.log( "Language selection..." );
+            })
+            .next()
+                .button({
+                    text: false,
+                    icons: {
+                        primary: "ui-icon-triangle-1-s"
+                    }
+                })
+                .click(function() {
+                    var menu = $( this ).parent().next().show().position({
+                        my: "left top",
+                        at: "left bottom",
+                        of: this
+                    });
+                    $( document ).one( "click", function() {
+                        menu.hide();
+                    });
+                    return false;
+                })
+                .parent()
+                    .buttonset()
+                    .next()
+                        .hide()
+                        .menu();
+		}
+	});
+
+	/* Attach page specific behavior on page load */
+	$(function() {
+		return new window[NS][SubClass]();
+	});
+}(window.jQuery, "WMC", "Base", "LocaleChanger"));
